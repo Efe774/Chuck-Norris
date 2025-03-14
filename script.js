81cb57dda1541659f7f3e08b2e95b9b5db39ffcf
Saim Efe Omag <saimefe774@gmail.com>
2025-03-14 12:30:48 +0300

    chuck norris sitesi

---
+++ script.js
@@ -0,0 +1,6 @@
+const şakaGetir = async () => {
+    const res = await fetch('https://api.chucknorris.io/jokes/random')
+    const random_joke = await res.json()
+    document.querySelector('p').innerText = random_joke.value
+    document.querySelector('small').style.display = 'none'
+}
